f29714c53139776a492d54ed6757ba50912824de
gokul <gokulmittu01@gmail.com>
2026-02-21 19:17:33 +0530

    'added_scroll_to_top'

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled'
+    })),
     provideAnimations()   // ✅ REQUIRED
   ]
 };
